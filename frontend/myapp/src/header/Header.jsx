3cb0bc9e29f5c7545c7cf3b0d754720b322beaed
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-12-04 16:19:20 +0200

    create Header and configure routing

---
+++ frontend/myapp/src/header/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { AppBar, Toolbar, Tabs, Tab } from "@mui/material";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import { Link } from "react-router-dom";
+
+const links = ["home", "diaries", "auth"];
+
+const Header = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <AppBar sx={{ bgcolor: "transparent", position: "sticky" }}>
+      <Toolbar>
+        <TravelExploreIcon sx={{ color: "black" }} />
+        <Tabs
+          value={value}
+          onChange={(event, value) => setValue(value)}
+          sx={{ ml: "auto", textDecoration: "none" }}
+        >
+          {links.map((link) => (
+            <Tab
+              LinkComponent={Link}
+              to={`/${link === "home" ? "" : link}`}
+              sx={{
+                textDecoration: "none",
+                ":hover": {
+                  textDecoration: "underline",
+                  textUnderlineOffset: "18px",
+                },
+              }}
+              key={link}
+              label={link}
+            />
+          ))}
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
